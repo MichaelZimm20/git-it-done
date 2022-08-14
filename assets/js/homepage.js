f62bd43537f3ab5ac01fe62ea329917ab8b09b63
DJMIKE20 <m.zimm20@gmail.com>
2022-08-14 14:41:30 -0400

    update request logic with dynamic variables

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,7 +1,16 @@
-var getUserRepos = function() {
-    console.log("function was called");
-    fetch ("http://api.github.com/users/octocat/repos");
-  };
-  
-  getUserRepos();
+var getUserRepos = function (user) {
+    // format the github api url
+    var apiUrl = "http://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url
+    fetch(apiUrl).then(function (response) {
+
+        response.json().then(function (data) {
+            console.log(data);
+        });
+    });
+};
+
+ 
+  getUserRepos('michaelzimm20');
 
